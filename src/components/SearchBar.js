b9d7f09d409c1c292900289415e9955d39004b20
Zeha Irawan <jangkaringbumi@gmail.com>
2020-05-29 17:48:27 +0700

    add state and onChange handler to SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchBar = () => {
+  const [keyword, setKeyword] = useState('');
+
+  const onChange = (e) => setKeyword(e.target.value);
+
   return (
     <div>
       <h3>SearchBar</h3>
-      <input placeholder="Find favorite recipe..."></input>
+      <input
+        value={keyword}
+        onChange={(e) => onChange(e)}
+        placeholder="Find your favorite recipe..."
+      ></input>
     </div>
   );
 };
