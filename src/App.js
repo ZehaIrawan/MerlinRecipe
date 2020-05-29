a284ed1403558582dd0d36129d6a551ebaa768df
Zeha Irawan <jangkaringbumi@gmail.com>
2020-05-29 17:21:14 +0700

    create SearchBar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Search...</p>
-        <input></input>
+        <SearchBar></SearchBar>
       </header>
     </div>
   );
---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return (
+    <div>
+      <h3>SearchBar</h3>
+      <input placeholder="Find favorite recipe..."></input>
+    </div>
+  );
+};
+
+export default SearchBar;
